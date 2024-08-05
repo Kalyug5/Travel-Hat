a4fcb6490fec2435825e42246502f82fed84eafe
Priyanshu <mishrapriyanshu555@gmail.com>
2024-08-05 21:58:16 +0530

    "fixes"

--- src/index.js
+++ src/index.js
@@ -5,13 +5,16 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./store";
+import { LoaderProvider } from "./app/stories/LoaderContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <LoaderProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </LoaderProvider>
   </React.StrictMode>
 );
 
